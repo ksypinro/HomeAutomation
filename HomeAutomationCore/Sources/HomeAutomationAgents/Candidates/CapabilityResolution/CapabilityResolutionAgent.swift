506300847fed4712bc668c3102c47854b849a29d
Kazi Samin Yeaser <samin@Kazis-MacBook-Pro.local>
2026-06-08 21:31:42 +0600

    feat: improve television and media capability resolution with expanded phrase matching and rules

--- HomeAutomationCore/Sources/HomeAutomationAgents/Candidates/CapabilityResolution/CapabilityResolutionAgent.swift
+++ HomeAutomationCore/Sources/HomeAutomationAgents/Candidates/CapabilityResolution/CapabilityResolutionAgent.swift
@@ -145,10 +145,11 @@ public struct CapabilityResolutionAgent: HomeAgent {
         let hasStatusIntent = families.contains(.statusQuery)
         let hasRoutineIntent = families.contains(.routine)
         let hasMediaIntent = families.contains(.media)
-        let hasTurnOn = containsAny(normalizedText, ["turn on", "switch on", "power on", "start"])
-        let hasTurnOff = containsAny(normalizedText, ["turn off", "switch off", "power off", "stop"])
+        let hasMediaControlIntent = containsAny(normalizedText, mediaControlPhrases)
+        let hasTurnOn = containsAny(normalizedText, powerOnPhrases)
+        let hasTurnOff = containsAny(normalizedText, powerOffPhrases)
 
-        if capability == "switch", hasPowerIntent || hasTurnOn || hasTurnOff {
+        if capability == "switch", !hasMediaControlIntent, hasPowerIntent || hasTurnOn || hasTurnOff {
             if hasTurnOff, commands.contains("off") {
                 return ("off", 0.95, ["Power intent and off phrase matched switch.off."])
             }
@@ -210,61 +211,61 @@ public struct CapabilityResolutionAgent: HomeAgent {
             }
         }
 
-        if hasMediaIntent || containsAny(normalizedText, ["tv", "television", "volume", "channel", "play", "pause", "mute", "input"]) {
-            if capability == "channel", normalizedText.contains("channel") {
-                if containsAny(normalizedText, ["next channel", "channel up", "increase channel", "raise channel", "move to next channel"]),
+        if hasMediaIntent || hasMediaControlIntent {
+            if capability == "channel" {
+                if containsAny(normalizedText, channelUpPhrases),
                    commands.contains("channelUp") {
                     return ("channelUp", 0.92, ["Media channel phrase matched channel.channelUp."])
                 }
-                if containsAny(normalizedText, ["previous channel", "prev channel", "last channel", "channel down", "decrease channel", "lower channel", "move to previous channel"]),
+                if containsAny(normalizedText, channelDownPhrases),
                    commands.contains("channelDown") {
                     return ("channelDown", 0.92, ["Media channel phrase matched channel.channelDown."])
                 }
-                if !state.slots.values.isEmpty, commands.contains("setChannel") {
+                if containsAny(normalizedText, setChannelPhrases), !state.slots.values.isEmpty, commands.contains("setChannel") {
                     return ("setChannel", 0.88, ["Media channel number matched channel.setChannel."])
                 }
             }
 
             if capability == "audioVolume" {
-                if normalizedText.contains("unmute"), commands.contains("unmute") {
-                    return ("unmute", 0.92, ["Media audio phrase matched audioVolume.unmute."])
+                if containsAny(normalizedText, unmutePhrases), commands.contains("unmute") {
+                    return ("unmute", 0.98, ["Media audio phrase matched audioVolume.unmute."])
                 }
-                if normalizedText.contains("mute"), commands.contains("mute") {
-                    return ("mute", 0.92, ["Media audio phrase matched audioVolume.mute."])
+                if containsAny(normalizedText, mutePhrases), commands.contains("mute") {
+                    return ("mute", 0.97, ["Media audio phrase matched audioVolume.mute."])
                 }
-                if containsAny(normalizedText, ["volume up", "turn up", "increase volume", "raise volume", "louder"]),
+                if containsAny(normalizedText, volumeUpPhrases),
                    commands.contains("volumeUp") {
                     return ("volumeUp", 0.88, ["Media audio phrase matched audioVolume.volumeUp."])
                 }
-                if containsAny(normalizedText, ["volume down", "turn down", "decrease volume", "lower volume", "quieter"]),
+                if containsAny(normalizedText, volumeDownPhrases),
                    commands.contains("volumeDown") {
                     return ("volumeDown", 0.88, ["Media audio phrase matched audioVolume.volumeDown."])
                 }
-                if normalizedText.contains("volume"), !state.slots.values.isEmpty, commands.contains("setVolume") {
+                if containsAny(normalizedText, setVolumePhrases), !state.slots.values.isEmpty, commands.contains("setVolume") {
                     return ("setVolume", 0.86, ["Media audio value matched audioVolume.setVolume."])
                 }
             }
 
             if capability == "mediaPlayback" {
-                if containsAny(normalizedText, ["fast forward", "skip ahead"]), commands.contains("fastForward") {
+                if containsAny(normalizedText, fastForwardPhrases), commands.contains("fastForward") {
                     return ("fastForward", 0.88, ["Media playback phrase matched mediaPlayback.fastForward."])
                 }
-                if containsAny(normalizedText, ["rewind", "go back"]), commands.contains("rewind") {
+                if containsAny(normalizedText, rewindPhrases), commands.contains("rewind") {
                     return ("rewind", 0.88, ["Media playback phrase matched mediaPlayback.rewind."])
                 }
-                if normalizedText.contains("pause"), commands.contains("pause") {
+                if containsAny(normalizedText, pausePhrases), commands.contains("pause") {
                     return ("pause", 0.88, ["Media playback phrase matched mediaPlayback.pause."])
                 }
-                if containsAny(normalizedText, ["play", "resume", "continue"]), commands.contains("play") {
-                    return ("play", 0.88, ["Media playback phrase matched mediaPlayback.play."])
-                }
-                if normalizedText.contains("stop"), commands.contains("stop") {
+                if containsAny(normalizedText, stopPlaybackPhrases), commands.contains("stop") {
                     return ("stop", 0.86, ["Media playback phrase matched mediaPlayback.stop."])
+                }
+                if containsAny(normalizedText, playPhrases), commands.contains("play") {
+                    return ("play", 0.96, ["Media playback phrase matched mediaPlayback.play."])
                 }
             }
 
             if capability == "mediaInputSource",
-               containsAny(normalizedText, ["input", "source", "hdmi", "usb", " av "]),
+               containsAny(normalizedText, inputSourcePhrases),
                commands.contains("setInputSource") {
                 return ("setInputSource", 0.86, ["Media input phrase matched mediaInputSource.setInputSource."])
             }
@@ -317,6 +318,126 @@ public struct CapabilityResolutionAgent: HomeAgent {
         }
         return evidence
     }
+
+    private static let powerOnPhrases = [
+        "turn on", "switch on", "power on", "power up", "start", "enable", "activate", "wake up",
+        "wake", "boot", "boot up", "fire up", "bring on", "put on", "start up", "energize"
+    ]
+
+    private static let powerOffPhrases = [
+        "turn off", "switch off", "power off", "power down", "shut off", "shut down", "disable",
+        "deactivate", "sleep", "put to sleep", "kill power", "stop power", "cut power", "switch down"
+    ]
+
+    private static let channelPhrases = [
+        "channel", "station", "program", "broadcast", "tune"
+    ]
+
+    private static let channelUpPhrases = [
+        "next channel", "channel up", "increase channel", "raise channel", "move to next channel",
+        "advance channel", "go up one channel", "step up a channel", "skip to next channel",
+        "flip to next channel", "channel forward", "tune up one channel", "scan up one channel",
+        "nudge channel up", "hop to next channel", "jump to next channel", "browse forward",
+        "cycle forward", "go forward one channel", "move channel forward"
+    ]
+
+    private static let channelDownPhrases = [
+        "previous channel", "prev channel", "last channel", "channel down", "decrease channel",
+        "lower channel", "move to previous channel", "go back a channel", "step down a channel",
+        "channel back", "tune down one channel", "scan down one channel", "hop back a channel",
+        "jump to prior channel", "flip to previous channel", "back one channel", "cycle backward",
+        "browse backward", "return to previous channel", "move channel backward"
+    ]
+
+    private static let setChannelPhrases = [
+        "channel", "station", "tune to channel", "go to channel", "jump to channel", "select channel",
+        "put channel", "set channel", "open channel", "show channel", "move to channel",
+        "switch to channel", "load channel", "choose channel", "watch channel"
+    ]
+
+    private static let setVolumePhrases = [
+        "volume", "sound level", "audio level", "speaker level", "loudness"
+    ]
+
+    private static let volumeUpPhrases = [
+        "volume up", "turn up", "increase volume", "raise volume", "louder", "boost volume",
+        "pump up volume", "make louder", "amplify volume", "add volume", "bump volume up",
+        "nudge volume up", "crank up volume", "more volume", "lift volume", "turn sound up",
+        "raise audio", "increase audio", "up the sound", "make tv louder"
+    ]
+
+    private static let volumeDownPhrases = [
+        "volume down", "turn down", "decrease volume", "lower volume", "quieter", "reduce volume",
+        "soften volume", "drop volume", "bring down volume", "make quieter", "lower audio",
+        "turn sound down", "less volume", "dial down volume", "nudge volume down", "cut volume",
+        "quiet down", "reduce audio", "lower sound", "make tv softer", "softer", "make softer"
+    ]
+
+    private static let mutePhrases = [
+        "mute", "silence", "turn sound off", "disable audio", "kill audio", "kill sound",
+        "cut audio", "cut the sound", "no sound", "quiet the tv", "shush", "shush the tv",
+        "turn audio off", "turn volume off", "remove sound", "block sound", "suppress audio",
+        "make silent", "silence audio", "silence the tv", "quiet the", "silent"
+    ]
+
+    private static let unmutePhrases = [
+        "unmute", "restore sound", "turn sound back on", "enable audio", "bring audio back",
+        "take off mute", "restore audio", "sound back on", "cancel mute", "remove mute",
+        "undo mute", "turn volume back on", "re enable sound", "let audio play", "unsilence",
+        "wake the sound", "resume sound", "restore tv volume", "restore volume", "restore",
+        "enable sound", "bring back sound"
+    ]
+
+    private static let playPhrases = [
+        "play", "resume", "continue", "start playback", "begin playback", "start playing",
+        "resume playback", "play media", "play video", "continue show", "resume show",
+        "start the program", "begin the video", "play the movie", "continue the movie",
+        "restart playback", "roll video", "run playback", "start streaming", "resume streaming"
+    ]
+
+    private static let pausePhrases = [
+        "pause", "hold playback", "freeze playback", "pause video", "pause show",
+        "suspend playback", "stop temporarily", "put playback on hold", "hold the show",
+        "pause media", "pause stream", "halt playback", "park the video", "pause program",
+        "pause the tv content", "take a break", "pause what is playing", "pause the movie",
+        "pause the current video", "hold current playback"
+    ]
+
+    private static let stopPlaybackPhrases = [
+        "stop playback", "stop media", "stop video", "end playback", "end show", "halt media",
+        "quit playback", "terminate playback", "cancel playback", "stop stream",
+        "shut playback down", "close media playback", "finish playback", "stop current video",
+        "end program", "cease playback", "cut playback", "stop what is playing",
+        "turn playback off", "halt the show"
+    ]
+
+    private static let fastForwardPhrases = [
+        "fast forward", "skip ahead", "jump ahead", "scan forward", "advance playback",
+        "go forward", "move forward", "skip forward", "advance video", "cue forward",
+        "seek ahead", "scrub forward", "forward the playback", "hurry ahead", "speed ahead",
+        "leap ahead", "wind forward", "move ahead in video", "push playback forward",
+        "fast forward playback"
+    ]
+
+    private static let rewindPhrases = [
+        "rewind", "go back", "skip back", "jump back", "scan backward", "move backward",
+        "rewind playback", "seek back", "scrub back", "back up video", "reverse playback",
+        "wind back", "return earlier", "go earlier", "move back in video", "step back",
+        "roll back playback", "backtrack playback", "pull playback back", "rewind video"
+    ]
+
+    private static let inputSourcePhrases = [
+        "input", "source", "hdmi", "usb", " av ", "select input", "choose input",
+        "change input", "switch input", "set input", "select source", "choose source",
+        "change source", "switch source", "set source", "route to", "use hdmi", "use usb",
+        "move source", "tv source"
+    ]
+
+    private static let mediaControlPhrases =
+        channelPhrases + channelUpPhrases + channelDownPhrases + setChannelPhrases +
+        setVolumePhrases + volumeUpPhrases + volumeDownPhrases + mutePhrases + unmutePhrases +
+        playPhrases + pausePhrases + stopPlaybackPhrases + fastForwardPhrases + rewindPhrases +
+        inputSourcePhrases + ["audio", "sound", "media", "movie", "video", "playback", "streaming", "show", "content"]
 
     private static func containsAny(_ text: String, _ needles: [String]) -> Bool {
         needles.contains { text.contains($0) }
